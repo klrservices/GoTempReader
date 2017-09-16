ebbbc77b3403c9e0632e1e2d606e0132798c6941
Lukasz Romaszewski <klrservices@users.noreply.github.com>
2017-09-16 19:44:27 +0200

    Introduced timer based polling from GoTemp probe.

--- GoTempIoTCSClient/src/main/java/pl/klr/workshops/iot/iotcs/IoTCSClient.java
+++ GoTempIoTCSClient/src/main/java/pl/klr/workshops/iot/iotcs/IoTCSClient.java
@@ -5,6 +5,10 @@ import oracle.iot.client.device.DirectlyConnectedDevice;
 import oracle.iot.client.device.VirtualDevice;
 import pl.klr.workshops.iot.sensors.vernier.GoTempProbe;
 
+import javax.usb.UsbException;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class IoTCSClient {
     private static final String URN = "urn:vernier:gotemp";
     private static final String TEMPERATURE_ATTRIBUTE = "Temperature";
@@ -24,22 +28,48 @@ public class IoTCSClient {
         boolean loopRead = true;
 
         GoTempProbe probe = new GoTempProbe();
+        IoTCSPublisher publisher = new IoTCSPublisher(probe, virtualDevice);
+        Timer timer = new Timer("GoTempTimer");
 
         probe.start();
+        timer.schedule(publisher, 100, 1000);
 
         while (loopRead) {
-            Double temperature = probe.poll();
-
-            System.out.println("Temp: "+temperature);
-            virtualDevice.set(TEMPERATURE_ATTRIBUTE, temperature);
-
-            Thread.sleep(1000);
+//            Double temperature = probe.poll();
 
             loopRead = System.in.available() == 0;
+
+            Thread.sleep(100);
         }
 
+
+        timer.cancel();
         probe.stop();
 
         dcd.close();
+    }
+
+    static class IoTCSPublisher extends TimerTask {
+        GoTempProbe probe;
+        VirtualDevice virtualDevice;
+
+        public IoTCSPublisher(GoTempProbe probe, VirtualDevice virtualDevice) {
+            this.probe = probe;
+            this.virtualDevice = virtualDevice;
+        }
+
+        @Override
+        public void run() {
+            Double temperature = null;
+            try {
+                temperature = probe.poll();
+
+                System.out.println("Temp: "+temperature);
+
+                virtualDevice.set(TEMPERATURE_ATTRIBUTE, temperature);
+            } catch (UsbException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
